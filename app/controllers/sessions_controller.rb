3f7560f689751e7f32c4517988dd517c8a4e30bd
Sheelove Dol <siesel26@gmail.com>
2021-01-18 03:23:30 -0800

    Add user-session functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,20 @@ class SessionsController < ApplicationController
 
   def create 
     #byebug
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id #to remain signed in for future request
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login detail"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
