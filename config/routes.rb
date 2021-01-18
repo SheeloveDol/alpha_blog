b229d96593793a06551e73d18eb838c41d70d0e7
Sheelove Dol <siesel26@gmail.com>
2021-01-17 19:59:40 -0800

    Add users resources and new forms and errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles #only: [:show, :index, :new, :create, :edit, :update, :destroy]<-->saying "resources :articles auto-creates all the REST-ful routes"
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
